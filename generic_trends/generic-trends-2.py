2ce843f084dac9531c6c268f14c1e6bd7d928ed9
Seb Bacon <seb.bacon@gmail.com>
2019-02-01 09:24:45 +0000

    Mention reasons why things might be prescribed as brands

--- generic_trends/generic-trends-2.py
+++ generic_trends/generic-trends-2.py
@@ -263,7 +263,13 @@ most_variance
 # 1. Why were they not already being prescribed as generics?
 # 2. What is driving the change?
 #
-# The only valid reason (I think) for (1) is where it's recommended not to - for example, things which come in radically different strengths but the format is similar (patches, opioids), things which work differently despite being equivalent at VMP level (modified-release stuff, things with narrow therapeutic index, things which come with devices (viz inhalers, syringes)), things with narrow therapeutic index / bioavailability variance, things which a different within generic class (e.g. skin preparations containing paraffin, foods).
+# Generally, things are not prescribed as generics  because:
+#
+#
+# * it is officially not recommended (for example, things which come in radically different strengths but the format is similar (patches, opioids), things which work differently despite being equivalent at VMP level (modified-release stuff, things with narrow therapeutic index, things which come with devices (viz inhalers, syringes)), things with narrow therapeutic index / bioavailability variance, things which a different within generic class (e.g. skin preparations containing paraffin, foods)
+# * it is avoided for similar, non-official reasons (opthamologists prescribing eye drops by brand because the bottle is more squeezy)
+# * because people are wary of prescribing generically because they're easy to confuse (this is common with contraceptive pills)
+# * because people just don't know the generic names (more common with combination products, products with long names)
 #
 # Just two of the things below are not in the class "should not substitute generically" (phenytoin, salbutamol); in these cases we should ask why they are increasingly being prescribed generically.  All the others it was always OK for them to be prescribed generically, so why weren't they?
 #
